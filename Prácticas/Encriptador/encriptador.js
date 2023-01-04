4e3c79810de8ff9112fa272fa1eb075a00f44ecf
ChristianDTC <chzepech@gmail.com>
2023-01-04 05:39:46 -0300

    agregue el boton copiar con sus funcionalidades, oculto y muestro el muñeco y el párrafo y boton copiar, también agregue una lectura del audio

--- Prácticas/Encriptador/encriptador.js
+++ Prácticas/Encriptador/encriptador.js
@@ -1,38 +1,133 @@
 var botonEncriptar = document.querySelector(".boton-encriptar");
 var botonDesencriptar = document.querySelector(".boton-desencriptar");
+var botonCopiar = document.querySelector(".boton-copiar");
 var munieco = document.querySelector(".contenedor-munieco");
 var h3 = document.querySelector(".contenedor-h3");
 var parrafo = document.querySelector(".contenedor-p");
+var txEncriptado = document.querySelector(".contenedor-tx-encriptado")
+var contenedorCopiar = document.querySelector(".contenedor-copiar")
 var textoEncriptado = document.querySelector(".texto-encriptado");
+var textarea = document.querySelector(".textarea");
 
 botonEncriptar.onclick = encriptar;
 botonDesencriptar.onclick = desencriptar;
+botonCopiar.onclick = copiarTexto;
+
+/*hago que el cursor este dentro del textarea */
+textarea.focus();
 
 function encriptar(){
     /*encriptar el texto*/
-    ocultarMunieco();
-    var textoRecuperado = recuperarTexto();
-    textoEncriptado.textContent = encriptarTexto(textoRecuperado);
+
+    /*controlo si tiene mayusculas para hacer el encriptado */
+    if (recuperarTexto() === true){
+        /*limpio el textarea, queda con el placeholder*/
+        textarea.value = "";
+        textarea.focus();
+        mostrarMunieco();
+    } else {
+        ocultarMunieco();
+        var textoRecuperado = recuperarTexto();
+        /*agrego el texto encriptado en el párrafo de texto-encriptado */
+        textoEncriptado.textContent = encriptarTexto(textoRecuperado);
+        /*genero un audio que lee el texto */
+        leer(textarea.value);
+        textarea.value = "";
+    } 
 }
 
 function desencriptar(){
     /*desencriptar el texto*/
-    ocultarMunieco();
-    var textoRecuperado = recuperarTexto();
-    textoEncriptado.textContent = desencriptarTexto(textoRecuperado);
+    if (recuperarTexto() === true){
+        textarea.value = "";
+        textarea.focus();
+        mostrarMunieco();
+    } else {
+        ocultarMunieco();
+        var textoRecuperado = recuperarTexto();
+        textoEncriptado.textContent = desencriptarTexto(textoRecuperado); 
+        leer(textarea.value);
+        textarea.value = "";
+    }
+}
+
+function copiarTexto(){
+    /*copio el texto del párrafo texto-encriptado */
+    var textoCopiado = textoEncriptado.textContent;
+    navigator.clipboard.writeText(textoCopiado);
+    leer(textoEncriptado.textContent);
+    mostrarMunieco();
+}
+
+function leer(texto){
+    /*hago que lea el texto que indico */
+    let mensaje = new SpeechSynthesisUtterance();
+    mensaje.text = texto;
+    mensaje.lang = "es-ES";
+    window.speechSynthesis.speak(mensaje);
 }
 
 function recuperarTexto(){
-    /*copiamos el texto a encriptar y lo retornamos*/
-    var textarea = document.querySelector(".textarea");
-    return textarea.value;
+    /*encripto el texto y lo retorno*/
+
+    /*variables de control */
+    var tieneMayusculas = false;
+    var tieneAcentos = false;
+    
+    /*recorro el texto para detectar si tiene mayuscular*/
+    for (var i=0; i < textarea.value.length; i++){
+        /*comparo cada letra con su mayuscula y discrimino los espacios en blanco */
+        if (textarea.value[i] === textarea.value[i].toUpperCase() & textarea.value[i] != " "){
+            alert("TEXTO SIN MAYUSCULAS");
+            tieneMayusculas = true;
+        }
+        
+        /*comparo si tiene acento, no incluyo mayusculas porque ya se descartan con el primer if */
+        if (textarea.value[i] === "á" ||
+            textarea.value[i] === "é" ||
+            textarea.value[i] === "í" ||
+            textarea.value[i] === "ó"||
+            textarea.value[i] === "ú"){
+            tieneAcentos = true;
+            alert("TEXTO SIN ACENTOS");
+            break;
+        }
+       
+    }
+    /*si tiene mayusculas o acentos retorno true sino el texto*/
+    if(tieneMayusculas || tieneAcentos){
+        return true;
+    } else {
+        return textarea.value;
+    }
 }
 
 function ocultarMunieco(){
-    /*oculta el munieco y el texto agregando la clase ocultar que creamos en el css*/
+    /*oculta el munieco y el texto agregando la clase ocultar que cree en el css*/
     munieco.classList.add("ocultar");
     h3.classList.add("ocultar");
     parrafo.classList.add("ocultar");
+    mostrarCopiar();
+}
+
+function mostrarMunieco(){
+    /*muestra el munieco y el texto eliminando la clase ocultar*/
+    munieco.classList.remove("ocultar");
+    h3.classList.remove("ocultar");
+    parrafo.classList.remove("ocultar");
+    ocultarCopiar();
+}
+
+function mostrarCopiar(){
+    /*muestra el texto y boton copiar eliminando la clase ocultar*/
+    txEncriptado.classList.remove("ocultar");
+    contenedorCopiar.classList.remove("ocultar");
+}
+
+function ocultarCopiar(){
+    /*oculta el texto y boton copiar agregando la clase ocultar que cree en el css*/
+    txEncriptado.classList.add("ocultar");
+    contenedorCopiar.classList.add("ocultar");
 }
 
 function encriptarTexto(mensaje){
