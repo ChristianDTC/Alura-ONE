ec13baef990ef1118f4e524a4d58451486cb3a7d
ChristianDTC <chzepech@gmail.com>
2023-02-23 23:03:40 -0300

    ejercicio de practica bytebank heredado cuenta

---
+++ Prácticas/Java Orientación a Objetos - ByteBank/Ejercicio de Practica ByteBankHeredadoCuenta/TestCuenta.java
@@ -0,0 +1,19 @@
+
+public class TestCuenta {
+	
+	public static void main(String[] args) {
+		
+
+	    CuentaCorriente cc = new CuentaCorriente(111, 111);
+	    cc.deposita(100.0);
+
+	    CuentaAhorro cp = new CuentaAhorro(222, 222);
+	    cp.deposita(100.0);
+	    
+	    cc.transfiere(10.0, cp);
+	    System.out.println("CC: " + cc.getSaldo());
+	    System.out.println("CP: " + cp.getSaldo());
+		
+	}
+
+}
